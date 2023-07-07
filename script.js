38acd4ba0cdf2740a6df4a90335c5f9d3aceb4c7
Reed <37314703+MrSamples@users.noreply.github.com>
2023-07-07 11:35:08 -0500

    made announcements/trending + dark mode

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cards = document.querySelectorAll(".card");
 const stars = document.querySelectorAll(".star");
 const eyes = document.querySelectorAll(".eye");
+const bell = document.querySelector(".notif")
+const modal = document.querySelector(".modal");
+const root = document.querySelector(":root");
 
 for (let i = 0; i < cards.length; i++) {
     stars[i].addEventListener("click", function (e) {
@@ -15,7 +18,6 @@ for (let i = 0; i < cards.length; i++) {
     });
 
     eyes[i].addEventListener("click", function (e) {
-        console.log(e.target.src);
         if (e.target.src.includes("/img/eye.svg")) {
             e.target.src = "./img/eye-filled.svg";
             cards[i].classList.add("blue");
@@ -25,4 +27,33 @@ for (let i = 0; i < cards.length; i++) {
             cards[i].classList.remove("blue");
         }
     });
+}
+
+bell.addEventListener("click", function (e) {
+    if (e.target.src.includes("/img/bell.svg"))
+        e.target.src = "./img/bell-filled.svg";
+    else
+        e.target.src = "./img/bell.svg";
+});
+
+modal.addEventListener("click", function (e) {
+    if (e.target.src.includes("/img/sun.svg")) {
+        e.target.src = "./img/moon.svg";
+        root.style.setProperty("--card-color", "#414952");
+        root.style.setProperty("--bg-color", "#21272e");
+        root.style.setProperty("--text-color", "#f8f8f0");
+        root.style.setProperty("--desc-color", "#e2e8f0");
+    }
+    else {
+        e.target.src = "./img/sun.svg";
+        root.style.setProperty("--card-color", "#f8f8f0");
+        root.style.setProperty("--bg-color", "#e2e8f0");
+        root.style.setProperty("--text-color", "#000000");
+        root.style.setProperty("--desc-color", "#4c4e50");
+    }
+});
+
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    console.log()
+    document.getElementsByClassName("modal")[0].src = "./img/moon.svg";
 }
